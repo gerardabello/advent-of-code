90f809dda7e2730f09102aca04a9bdcd56fd3b91
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-09 09:06:45 +0100

    add day 9

--- src/d9p1.rs
+++ src/d9p1.rs
@@ -1,3 +1,39 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+fn is_sum_of_two_previous(num: u64, previous: &[u64]) -> bool {
+    for p1 in previous {
+        for p2 in previous {
+            if p1 + p2 == num && p1 != p2 {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+pub fn find_first_invalid(numbers: &[u64], preamble: usize) -> Option<u64> {
+    for i in preamble..numbers.len() {
+        let previous = &numbers[(i - preamble)..i];
+        let num = numbers[i];
+
+        if !is_sum_of_two_previous(num, previous) {
+            return Some(num);
+        }
+    }
+
+    None
+}
+
+pub fn solve(input: &str) -> String {
+    let preamble = 25;
+
+    let numbers: Vec<u64> = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+
+    match find_first_invalid(&numbers, preamble) {
+        Some(n) => n.to_string(),
+        None => String::from("Not found"),
+    }
 }
--- src/d9p2.rs
+++ src/d9p2.rs
@@ -1,3 +1,39 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use crate::d9p1::find_first_invalid;
+
+fn find_range_that_sums_invalid(numbers: &[u64], invalid: u64) -> Option<&[u64]> {
+    for i in 0..numbers.len() {
+        for j in i..numbers.len() {
+            let range = &numbers[i..j];
+            if range.iter().sum::<u64>() == invalid {
+                return Some(range);
+            }
+        }
+    }
+
+    None
+}
+
+pub fn solve(input: &str) -> String {
+    let preamble = 25;
+
+    let numbers: Vec<u64> = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+
+    let first_invalid = match find_first_invalid(&numbers, preamble) {
+        Some(n) => n,
+        None => panic!("First invalid number not found"),
+    };
+
+    let range = match find_range_that_sums_invalid(&numbers, first_invalid) {
+        Some(n) => n,
+        None => panic!("Range not found"),
+    };
+
+    let min_in_range = range.iter().min().unwrap();
+    let max_in_range = range.iter().max().unwrap();
+
+    (min_in_range + max_in_range).to_string()
 }
