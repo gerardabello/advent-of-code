edb1780ce76d74a6c6d31d7893212e4ef5599b4a
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-13 17:21:10 +0100

    day 13 solution

--- src/d13p1.rs
+++ src/d13p1.rs
@@ -1,3 +1,54 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use nom::{
+    branch::alt,
+    bytes::complete::{is_a, tag},
+    character::complete::digit1,
+    combinator::{map, map_res, recognize},
+    multi::separated_list1,
+    IResult,
+};
+
+fn parse_unsigned_integer_64(input: &str) -> IResult<&str, u64> {
+    map_res(recognize(digit1), str::parse)(input)
+}
+
+fn parse_bus_id(input: &str) -> IResult<&str, Option<u64>> {
+    let parser = alt((tag("x"), is_a("0123456789")));
+
+    map(parser, |s| match s {
+        "x" => None,
+        num => Some(num.parse::<u64>().unwrap()),
+    })(input)
+}
+
+pub fn parse_input(input: &str) -> IResult<&str, (u64, Vec<Option<u64>>)> {
+    let (input, arrival_time) = parse_unsigned_integer_64(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, result_ints) = separated_list1(tag(","), parse_bus_id)(input)?;
+
+    Ok((input, (arrival_time, result_ints)))
+}
+
+pub fn next_bus_wait (timestamp: u64, bus_id: u64) -> u64 {
+    bus_id - (timestamp % bus_id)
+}
+
+fn first_bus(timestamp: u64, bus_ids: &[u64]) -> Option<(u64, u64)> {
+    bus_ids
+        .iter()
+        .map(|id| (*id, next_bus_wait(timestamp, *id)))
+        .min_by(|a, b| a.1.cmp(&b.1))
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, (arrival_time, bus_ids_o)) = parse_input(input).unwrap();
+
+    let bus_ids: Vec<u64> = bus_ids_o
+        .iter()
+        .filter(|o| o.is_some())
+        .map(|o| o.unwrap())
+        .collect();
+
+    let (id, wait_time) = first_bus(arrival_time, &bus_ids).unwrap();
+
+    (id * wait_time).to_string()
 }
--- src/d13p2.rs
+++ src/d13p2.rs
@@ -1,3 +1,64 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use crate::d13p1::{next_bus_wait, parse_input};
+
+
+fn is_next_bus_at_offset(timestamp: u64, bus_id: u64, offset: u64) -> bool {
+    let wait = next_bus_wait(timestamp, bus_id);
+
+    // There could be an earlier bus, but any (timestamp + wait + n * busID) is also valid.
+    offset % bus_id == wait
+}
+
+// Checks if the timestamp is valid, if not, it returns the last bus that matched the offset.
+fn check_timestamp_for_bus(ids_offsets: &[(u64, u64)], bus_index: usize, timestamp: u64) -> Result<(), u64> {
+    if bus_index >= ids_offsets.len() - 1 {
+        return Ok(());
+    }
+
+    let (next_bus, next_offset) = ids_offsets[bus_index + 1];
+    if is_next_bus_at_offset(timestamp, next_bus, next_offset) {
+        return check_timestamp_for_bus(ids_offsets, bus_index + 1,timestamp);
+    }
+
+    Err(bus_index as u64)
+}
+
+fn find_timestamp_matching_offsets(ids_offsets: &[(u64, u64)]) -> u64 {
+
+    let mut base = ids_offsets[0].0;
+    let mut base_i = 0;
+
+    let mut timestamp = base;
+
+    loop {
+        let ret = check_timestamp_for_bus(ids_offsets, 0, timestamp);
+
+        match ret {
+            Ok(_) => return timestamp,
+            Err(i) => {
+                if i > base_i {
+                    // if the timestamp has a new bus that matches, multiply it's ID, as we know
+                    // that the same patters will only repeat in (bus1ID * bus2ID * ...)
+                    base_i = i;
+                    base *= ids_offsets[i as usize].0;
+                }
+            }
+        }
+
+        timestamp += base;
+    }
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, (_, bus_ids)) = parse_input(input).unwrap();
+
+    // Vec of id,offset
+    let ids_offsets: Vec<(u64, u64)> = bus_ids
+        .iter()
+        .enumerate()
+        .filter(|en| en.1.is_some())
+        .map(|en| (en.0 as u64, en.1.unwrap()))
+        .map(|(o, i)| (i, o))
+        .collect();
+
+    find_timestamp_matching_offsets(&ids_offsets).to_string()
 }
