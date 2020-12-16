d25f7158cb16c9cecf21835fb89ac35f5b885c08
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-16 09:30:07 +0100

    add day 16 solution

--- src/d16p1.rs
+++ src/d16p1.rs
@@ -1,3 +1,67 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use nom::{
+    bytes::complete::{tag, take_until},
+    character::complete::digit1,
+    combinator::{map_res},
+    multi::separated_list1,
+    IResult,
+};
+
+pub type Ticket = Vec<u32>;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Rule {
+    pub name: String,
+    pub ranges: Vec<(u32, u32)>,
+}
+
+impl Rule {
+    pub fn validate(&self, n: u32) -> bool{
+        self.ranges.iter().any(|r| n>= r.0 && n<= r.1)
+    }
+}
+
+fn parse_i32(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
+
+fn parse_ticket(input: &str) -> IResult<&str, Ticket> {
+    separated_list1(tag(","), parse_i32)(input)
+}
+
+fn parse_range(input: &str) -> IResult<&str, (u32, u32)> {
+    let (input, min) = parse_i32(input)?;
+    let (input, _) = tag("-")(input)?;
+    let (input, max) = parse_i32(input)?;
+
+    Ok((input,(min,max)))
+}
+
+fn parse_rule(input: &str) -> IResult<&str, Rule> {
+    let (input, name) = take_until(":")(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, _) = tag(" ")(input)?;
+    let (input, ranges) = separated_list1(tag(" or "), parse_range)(input)?;
+
+    Ok((input, Rule { name: name.to_owned(), ranges }))
+}
+
+pub fn parse_input(input: &str) -> IResult<&str, (Vec<Rule>, Ticket, Vec<Ticket>)> {
+    let (input, rules) = separated_list1(tag("\n"), parse_rule)(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, _) = tag("your ticket:\n")(input)?;
+    let (input, my_ticket) = parse_ticket(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, _) = tag("nearby tickets:\n")(input)?;
+    let (input, tickets) = separated_list1(tag("\n"), parse_ticket)(input)?;
+
+    Ok((input, (rules, my_ticket, tickets)))
+}
+
+pub fn solve(input: &str) -> String {
+
+    let (_ ,(rules, _, tickets)) = parse_input(input).unwrap();
+
+    tickets.iter().flatten().filter(|n| rules.iter().all(|r| !r.validate(**n))).sum::<u32>().to_string()
 }
--- src/d16p2.rs
+++ src/d16p2.rs
@@ -1,3 +1,90 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use crate::d16p1::{parse_input, Rule, Ticket};
+
+fn find_valid_rules<'a>(rules: &'a [Rule], numbers: &[u32]) -> Vec<String> {
+    rules
+        .iter()
+        .filter(|rule| numbers.iter().all(|n| rule.validate(*n)))
+        .map(|r| r.name.clone())
+        .collect()
+}
+
+fn find_order_of_fields(rules: &[Rule], tickets: &[&Ticket]) -> Vec<String> {
+    let n_fields = tickets[0].len();
+    let mut ret: Vec<(usize, String)> = Vec::new();
+    let mut matched_rules: Vec<(usize, Vec<String>)> = Vec::new();
+
+    // For each index, find all field that match
+    for i in 0..n_fields {
+        let numbers_in_i: Vec<u32> = tickets.iter().map(|t| t[i]).collect();
+
+        matched_rules.push((i, find_valid_rules(&rules, &numbers_in_i)));
+    }
+
+    // Sort them by number of matched fields, we want to first assign the indexes that have less
+    // possibilities.
+    matched_rules.sort_by(|(_, ma), (_, mb)| ma.len().cmp(&mb.len()));
+
+    // For each index, assign the first possibility, and remove that field from the rest
+    for mi in 0..matched_rules.len() {
+        let (index, matches) = &matched_rules[mi];
+        assert!(!matches.is_empty());
+
+        let field = &matches[0];
+
+        ret.push((*index, field.clone()));
+
+        matched_rules = matched_rules
+            .iter()
+            .map(|(i, matches)| {
+                (
+                    *i,
+                    matches
+                        .iter()
+                        .filter(|m| *m != field)
+                        .cloned()
+                        .collect::<Vec<String>>(),
+                )
+            })
+            .collect();
+    }
+
+    // Sort the assigned field by index order
+    ret.sort_by(|(ia, _), (ib, _)| ia.cmp(&ib));
+
+    ret.iter().map(|(_, field)| field).cloned().collect()
+}
+
+fn is_valid_ticket(rules: &[Rule], ticket: &Ticket) -> bool {
+    ticket.iter().all(|n| rules.iter().any(|r| r.validate(*n)))
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, (rules, my_ticket, tickets)) = parse_input(input).unwrap();
+
+    let mut valid_tickets: Vec<&Ticket> = tickets
+        .iter()
+        .filter(|t| is_valid_ticket(&rules, t))
+        .collect();
+
+
+    valid_tickets.push(&my_ticket);
+
+    let order = find_order_of_fields(&rules, &valid_tickets);
+
+
+    let indexes_to_multiply: Vec<usize> = order
+        .iter()
+        .enumerate()
+        .filter(|(_, name)| name.starts_with("departure"))
+        .map(|(i, _)| i)
+        .collect();
+
+
+    my_ticket
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| indexes_to_multiply.contains(i))
+        .map(|(_, val)| *val as u64)
+        .product::<u64>()
+        .to_string()
 }
