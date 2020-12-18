0226186bc563135744cdff38fc1be0791e60763b
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-18 09:45:35 +0100

    add day 18 solution

--- src/d18p1.rs
+++ src/d18p1.rs
@@ -1,3 +1,66 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use nom::IResult;
+
+enum Operator {
+    Sum,
+    Multiplication,
+}
+
+fn parse_math_expression(
+    input: &str,
+    acc: u64,
+    pending_operation: Option<Operator>,
+) -> IResult<&str, u64> {
+    let take_o = input.get(0..1);
+
+    match take_o {
+        Some(c) => {
+            let rest = &input[1..];
+            match c {
+                " " => parse_math_expression(rest, acc, pending_operation),
+                "(" => {
+                    let (r, pa) = parse_math_expression(rest, 0, Some(Operator::Sum)).unwrap();
+                    match pending_operation {
+                        Some(Operator::Multiplication) => parse_math_expression(r, acc * pa, None),
+                        Some(Operator::Sum) => parse_math_expression(r, acc + pa, None),
+                        None => unreachable!(),
+                    }
+                }
+                ")" => Ok((rest, acc)),
+                "*" => parse_math_expression(rest, acc, Some(Operator::Multiplication)),
+                "+" => parse_math_expression(rest, acc, Some(Operator::Sum)),
+                digit => {
+                    let num = digit.parse::<u64>().expect("expect it to be a digit");
+                    match pending_operation {
+                        Some(Operator::Multiplication) => {
+                            parse_math_expression(rest, acc * num, None)
+                        }
+                        Some(Operator::Sum) => parse_math_expression(rest, acc + num, None),
+                        None => unreachable!(),
+                    }
+                }
+            }
+        }
+        None => Ok((input, acc)),
+    }
+}
+
+pub fn parse_math_expressions(input: &str) -> Vec<u64> {
+    input
+        .split('\n')
+        // remove parenthesis
+        //.map(|line| line.chars().filter(|c| *c != '(' && *c != ')').collect())
+        .map(|line| {
+            parse_math_expression(line, 0, Some(Operator::Sum))
+                .unwrap()
+                .1
+        })
+        .collect()
+}
+
+pub fn solve(input: &str) -> String {
+    let numbers = parse_math_expressions(input);
+
+    println!("{:?}", numbers);
+
+    numbers.iter().sum::<u64>().to_string()
 }
--- src/d18p2.rs
+++ src/d18p2.rs
@@ -1,3 +1,75 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use regex::{Captures, Regex};
+
+pub fn pass_parenthesis(input: &str) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\(([\s\d\+\*]+)\)").unwrap();
+    }
+
+    let result = RE.replace(input, |caps: &Captures| {
+        let s = &caps[1];
+        passes(s)
+    });
+
+    result.as_ref().to_owned()
+}
+
+pub fn pass_sum(input: &str) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"([\d]+)\s\+\s([\d]+)").unwrap();
+    }
+    let result = RE.replace(input, |caps: &Captures| {
+        let n1 = &caps[1].parse::<u64>().unwrap();
+        let n2 = &caps[2].parse::<u64>().unwrap();
+        format!("{}", n1 + n2)
+    });
+
+    result.as_ref().to_owned()
+}
+
+pub fn pass_mul(input: &str) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"([\d]+)\s\*\s([\d]+)").unwrap();
+    }
+    let result = RE.replace(input, |caps: &Captures| {
+        let n1 = &caps[1].parse::<u64>().unwrap();
+        let n2 = &caps[2].parse::<u64>().unwrap();
+        format!("{}", n1 * n2)
+    });
+
+    result.as_ref().to_owned()
+}
+
+pub fn passes(input: &str) -> String {
+    let mut s = input.to_string();
+    loop {
+
+        let n_s = pass_parenthesis(&s);
+        if n_s != s {
+            s = n_s;
+            continue;
+        }
+        let n_s = pass_sum(&s);
+        if n_s != s {
+            s = n_s;
+            continue;
+        }
+        let n_s = pass_mul(&s);
+        if n_s != s {
+            s = n_s;
+            continue;
+        }
+
+        return s;
+    }
+}
+
+pub fn solve(input: &str) -> String {
+    let numbers: Vec<u64> = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(passes)
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+
+    numbers.iter().sum::<u64>().to_string()
 }
