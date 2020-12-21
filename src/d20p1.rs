343f6a5cdf25b6bc16d8f1ee0f7b4d82f0f21f64
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-21 10:58:09 +0100

    add day 20 solution
    What a mess

--- src/d20p1.rs
+++ src/d20p1.rs
@@ -1,3 +1,275 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use std::collections::HashSet;
+use std::convert::TryInto;
+use std::fmt;
+
+use nom::{
+    bytes::complete::{tag, take, take_until},
+    character::complete::digit1,
+    combinator::map_res,
+    multi::separated_list1,
+    IResult,
+};
+
+pub const IMAGE_PIECE_SIZE: usize = 10;
+
+#[derive(Clone, Debug)]
+pub enum Rotation {
+    Deg0,
+    Deg90,
+    Deg180,
+    Deg270,
+}
+
+pub struct ImageData {
+    pub id: u32,
+    data: [[bool; IMAGE_PIECE_SIZE]; IMAGE_PIECE_SIZE],
+}
+
+#[derive(Clone)]
+pub struct ImagePiece {
+    pub id: u32,
+    pub rotation: Rotation,
+    pub flip_v: bool,
+    pub flip_h: bool,
+}
+
+impl fmt::Debug for ImagePiece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.id).expect("Print should work");
+        match self.rotation {
+            Rotation::Deg0 => write!(f, "r0").expect("Print should work"),
+            Rotation::Deg90 => write!(f, "r1").expect("Print should work"),
+            Rotation::Deg180 => write!(f, "r2").expect("Print should work"),
+            Rotation::Deg270 => write!(f, "r3").expect("Print should work"),
+        }
+        if self.flip_h {
+            write!(f, "h").expect("Print should work");
+        }
+        if self.flip_v {
+            write!(f, "v").expect("Print should work");
+        }
+        Ok(())
+    }
+}
+
+    fn rotate_coords_90(size: usize, coords: (usize, usize)) -> (usize, usize) {
+        (coords.1, size - 1 - coords.0)
+    }
+
+    pub fn rotate_coords(size: usize, coords: (usize, usize), rotation: &Rotation) -> (usize, usize) {
+        let x = coords.0;
+        let y = coords.1;
+        match rotation {
+            Rotation::Deg0 => (x, y),
+            Rotation::Deg90 => rotate_coords_90(size,(x, y)),
+            Rotation::Deg180 => rotate_coords_90(size,rotate_coords_90(size,(x, y))),
+            Rotation::Deg270 => rotate_coords_90(size,rotate_coords_90(size,
+                rotate_coords_90(size,(x, y)),
+            )),
+        }
+    }
+
+    pub fn flip_horizontal(size: usize, coords: (usize, usize), flip: bool) -> (usize, usize) {
+        let x = coords.0;
+        let y = coords.1;
+        if flip {
+            return (size - 1 - x, y);
+        }
+
+        (x, y)
+    }
+
+    pub fn flip_vertical(size: usize, coords: (usize, usize), flip: bool) -> (usize, usize) {
+        let x = coords.0;
+        let y = coords.1;
+        if flip {
+            return (x, size - 1 - y);
+        }
+
+        (x, y)
+    }
+
+impl ImagePiece {
+
+    fn rotate_coords(coords: (usize, usize), rotation: &Rotation) -> (usize, usize) {
+        rotate_coords(IMAGE_PIECE_SIZE, coords, rotation)
+    }
+
+    fn flip_horizontal(coords: (usize, usize), flip: bool) -> (usize, usize) {
+        flip_horizontal(IMAGE_PIECE_SIZE, coords, flip)
+    }
+
+    fn flip_vertical(coords: (usize, usize), flip: bool) -> (usize, usize) {
+        flip_vertical(IMAGE_PIECE_SIZE, coords, flip)
+    }
+
+    pub fn get_data_at_pos(&self, data: &ImageData, x: usize, y: usize) -> Option<bool> {
+        if x >= IMAGE_PIECE_SIZE || y >= IMAGE_PIECE_SIZE {
+            return None;
+        }
+
+        assert!(self.id == data.id);
+
+        let (rx, ry) = ImagePiece::flip_vertical(
+            ImagePiece::flip_horizontal(
+                ImagePiece::rotate_coords((x, y), &self.rotation),
+                self.flip_h,
+            ),
+            self.flip_v,
+        );
+
+        Some(data.data[ry][rx])
+    }
+}
+
+fn vec_into_image_size_array<T>(v: Vec<T>) -> [T; IMAGE_PIECE_SIZE] {
+    v.try_into().unwrap_or_else(|v: Vec<T>| {
+        panic!(
+            "Expected a Vec of length {} but it was {}",
+            IMAGE_PIECE_SIZE,
+            v.len()
+        )
+    })
+}
+
+fn parse_data_row(input: &str) -> IResult<&str, [bool; IMAGE_PIECE_SIZE]> {
+    let (input, row) = take_until("\n")(input)?;
+
+    let vector: Vec<_> = row.chars().map(|c| c == '#').collect();
+
+    Ok((input, vec_into_image_size_array(vector)))
+}
+
+fn parse_data(input: &str) -> IResult<&str, [[bool; IMAGE_PIECE_SIZE]; IMAGE_PIECE_SIZE]> {
+    let (input, to_parse) = take(IMAGE_PIECE_SIZE * IMAGE_PIECE_SIZE + (IMAGE_PIECE_SIZE))(input)?;
+    let (_, vector) = separated_list1(tag("\n"), parse_data_row)(to_parse)?;
+
+    Ok((input, vec_into_image_size_array(vector)))
+}
+
+fn parse_header(input: &str) -> IResult<&str, u32> {
+    let (input, _) = tag("Tile ")(input)?;
+    let (input, id) = map_res(digit1, |s: &str| s.parse::<u32>())(input)?;
+    let (input, _) = tag(":")(input)?;
+
+    Ok((input, id))
+}
+
+fn parse_image_data(input: &str) -> IResult<&str, ImageData> {
+    let (input, id) = parse_header(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, data) = parse_data(input)?;
+
+    Ok((input, ImageData { id, data }))
+}
+
+pub fn parse_input(input: &str) -> IResult<&str, Vec<ImageData>> {
+    separated_list1(tag("\n"), parse_image_data)(input)
+}
+
+pub fn extract_top_border(piece: &ImagePiece, data: &ImageData) -> [bool; IMAGE_PIECE_SIZE] {
+    let mut ret: [bool; IMAGE_PIECE_SIZE] = [false; IMAGE_PIECE_SIZE];
+    for i in 0..IMAGE_PIECE_SIZE {
+        ret[i] = piece.get_data_at_pos(data, i, 0).unwrap();
+    }
+
+    ret
+}
+
+pub fn extract_bottom_border(piece: &ImagePiece, data: &ImageData) -> [bool; IMAGE_PIECE_SIZE] {
+    let mut ret: [bool; IMAGE_PIECE_SIZE] = [false; IMAGE_PIECE_SIZE];
+    for i in 0..IMAGE_PIECE_SIZE {
+        ret[i] = piece
+            .get_data_at_pos(data, i, IMAGE_PIECE_SIZE - 1)
+            .unwrap();
+    }
+
+    ret
+}
+
+pub fn extract_left_border(piece: &ImagePiece, data: &ImageData) -> [bool; IMAGE_PIECE_SIZE] {
+    let mut ret: [bool; IMAGE_PIECE_SIZE] = [false; IMAGE_PIECE_SIZE];
+    for i in 0..IMAGE_PIECE_SIZE {
+        ret[i] = piece.get_data_at_pos(data, 0, i).unwrap();
+    }
+
+    ret
+}
+
+pub fn extract_right_border(piece: &ImagePiece, data: &ImageData) -> [bool; IMAGE_PIECE_SIZE] {
+    let mut ret: [bool; IMAGE_PIECE_SIZE] = [false; IMAGE_PIECE_SIZE];
+    for i in 0..IMAGE_PIECE_SIZE {
+        ret[i] = piece
+            .get_data_at_pos(data, IMAGE_PIECE_SIZE - 1, i)
+            .unwrap();
+    }
+
+    ret
+}
+
+pub fn extract_borders(piece: &ImagePiece, data: &ImageData) -> [[bool; IMAGE_PIECE_SIZE]; 4] {
+    [
+        extract_left_border(piece, data),
+        extract_right_border(piece, data),
+        extract_top_border(piece, data),
+        extract_bottom_border(piece, data),
+    ]
+}
+
+pub fn flip_image_size_array<T: Copy>(a: &[T; IMAGE_PIECE_SIZE]) -> [T; IMAGE_PIECE_SIZE] {
+    vec_into_image_size_array(a.iter().rev().cloned().collect())
+}
+
+pub fn find_edges<'a>(
+    pieces: &'a [ImagePiece],
+    datas: &[ImageData],
+    only_corners: bool,
+) -> Vec<&'a ImagePiece> {
+    let mut corners: Vec<&ImagePiece> = Vec::new();
+
+    for piece in pieces {
+        let mut set: HashSet<[bool; IMAGE_PIECE_SIZE]> = HashSet::new();
+
+        for other_piece in pieces.iter().filter(|p| p.id != piece.id) {
+            for border in &extract_borders(
+                other_piece,
+                datas.iter().find(|d| d.id == other_piece.id).unwrap(),
+            ) {
+                set.insert(*border);
+                set.insert(flip_image_size_array(border));
+            }
+        }
+
+        let borders: [[bool; 10]; 4] =
+            extract_borders(piece, datas.iter().find(|d| d.id == piece.id).unwrap());
+        let found = borders.iter().filter(|b| set.contains(*b)).count();
+        if only_corners {
+            if found <= 2 {
+                corners.push(piece);
+            }
+        } else if found <= 3 {
+            corners.push(piece);
+        }
+    }
+
+    corners
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, datas) = parse_input(input).unwrap();
+
+    let pieces = datas.iter().map(|d| ImagePiece {
+        id: d.id,
+        rotation: Rotation::Deg0,
+        flip_h: false,
+        flip_v: false,
+    }).collect::<Vec<_>>();
+
+    let corners = find_edges(&pieces, &datas, true);
+
+    corners
+        .iter()
+        .map(|p| p.id as u128)
+        .product::<u128>()
+        .to_string()
 }
--- src/d20p2.rs
+++ src/d20p2.rs
@@ -1,3 +1,419 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use std::collections::{HashMap, HashSet};
+use std::iter::Iterator;
+
+use crate::d20p1::{
+    rotate_coords, flip_horizontal, flip_vertical,
+    extract_borders, extract_bottom_border, extract_left_border, extract_right_border,
+    extract_top_border, find_edges, flip_image_size_array, parse_input, ImageData, ImagePiece,
+    Rotation, IMAGE_PIECE_SIZE,
+};
+
+type Border = [bool; IMAGE_PIECE_SIZE];
+
+fn merge_vecs<T: Clone>(mut a: Vec<T>, mut b: Vec<T>) -> Vec<T> {
+    a.append(&mut b);
+
+    a
+}
+
+#[derive(Clone)]
+pub struct Image {
+    width: usize,
+    pieces: Vec<Option<ImagePiece>>,
+}
+
+impl Image {
+    fn new(width: usize) -> Self {
+        Image {
+            width,
+            pieces: vec![None; width * width],
+        }
+    }
+
+    fn form_complete_image(&self, datas: &[ImageData]) -> Vec<Vec<bool>> {
+        let no_border_width = IMAGE_PIECE_SIZE - 2;
+        let mut ret = vec![vec![false; self.width * no_border_width]; self.width * no_border_width];
+
+        for ix in 0..self.width {
+            for iy in 0..self.width {
+                let piece = self.get_piece_at_pos(ix, iy).unwrap();
+                let data = datas.iter().find(|d| d.id == piece.id).unwrap();
+                for x in 0..no_border_width {
+                    for y in 0..no_border_width {
+                        let d = piece.get_data_at_pos(data, 1 + x, 1 + y).unwrap();
+
+                        ret[iy * no_border_width + y][ix * no_border_width + x] = d;
+                    }
+                }
+            }
+        }
+        ret
+    }
+
+    fn get_neiguhouring_borders(&self, x: usize, y: usize, datas: &[ImageData]) -> Vec<Border> {
+        let mut ret = Vec::new();
+
+        let top_neighbour = self.get_top_neighbour(x, y);
+
+        if let Some(n) = top_neighbour {
+            ret.push(extract_bottom_border(
+                n,
+                datas.iter().find(|d| d.id == n.id).unwrap(),
+            ));
+        }
+
+        let left_neighbour = self.get_left_neighbour(x, y);
+
+        if let Some(n) = left_neighbour {
+            ret.push(extract_right_border(
+                n,
+                datas.iter().find(|d| d.id == n.id).unwrap(),
+            ));
+        }
+
+        let bottom_neighbout = self.get_bottom_neighbour(x, y);
+
+        if let Some(n) = bottom_neighbout {
+            ret.push(extract_top_border(
+                n,
+                datas.iter().find(|d| d.id == n.id).unwrap(),
+            ));
+        }
+
+        let right_neighbour = self.get_right_neighbour(x, y);
+
+        if let Some(n) = right_neighbour {
+            ret.push(extract_left_border(
+                n,
+                datas.iter().find(|d| d.id == n.id).unwrap(),
+            ));
+        }
+
+        ret
+    }
+
+    fn can_add_piece_at(
+        &self,
+        x: usize,
+        y: usize,
+        piece: &ImagePiece,
+        datas: &[ImageData],
+    ) -> bool {
+        let top_neighbour = self.get_top_neighbour(x, y);
+
+        if let Some(n) = top_neighbour {
+            if !self.compare_pieces_vertically(n, piece, datas) {
+                return false;
+            }
+        }
+
+        let left_neighbour = self.get_left_neighbour(x, y);
+
+        if let Some(n) = left_neighbour {
+            if !self.compare_pieces_horizontally(n, piece, datas) {
+                return false;
+            }
+        }
+
+        let bottom_neighbout = self.get_bottom_neighbour(x, y);
+
+        if let Some(n) = bottom_neighbout {
+            if !self.compare_pieces_vertically(piece, n, datas) {
+                return false;
+            }
+        }
+
+        let right_neighbour = self.get_right_neighbour(x, y);
+
+        if let Some(n) = right_neighbour {
+            if !self.compare_pieces_horizontally(piece, n, datas) {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn compare_pieces_horizontally(
+        &self,
+        left: &ImagePiece,
+        right: &ImagePiece,
+        datas: &[ImageData],
+    ) -> bool {
+        for y in 0..IMAGE_PIECE_SIZE {
+            if left.get_data_at_pos(
+                datas.iter().find(|d| d.id == left.id).unwrap(),
+                IMAGE_PIECE_SIZE - 1,
+                y,
+            ) != right.get_data_at_pos(datas.iter().find(|d| d.id == right.id).unwrap(), 0, y)
+            {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn compare_pieces_vertically(
+        &self,
+        top: &ImagePiece,
+        bottom: &ImagePiece,
+        datas: &[ImageData],
+    ) -> bool {
+        for x in 0..IMAGE_PIECE_SIZE {
+            if top.get_data_at_pos(
+                datas.iter().find(|d| d.id == top.id).unwrap(),
+                x,
+                IMAGE_PIECE_SIZE - 1,
+            ) != bottom.get_data_at_pos(datas.iter().find(|d| d.id == bottom.id).unwrap(), x, 0)
+            {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn set_piece_at_pos(&mut self, x: usize, y: usize, piece: ImagePiece) {
+        if x >= self.width || y >= self.width {
+            panic!("wrong indexes at set_piece_at_pos");
+        }
+
+        let index = y * self.width + x;
+        self.pieces[index] = Some(piece);
+    }
+
+    fn get_piece_at_pos(&self, x: usize, y: usize) -> Option<&ImagePiece> {
+        if x >= self.width || y >= self.width {
+            return None;
+        }
+
+        let index = y * self.width + x;
+        match self.pieces.get(index).unwrap() {
+            Some(p) => Some(p),
+            None => None,
+        }
+    }
+
+    fn get_top_neighbour(&self, x: usize, y: usize) -> Option<&ImagePiece> {
+        if y == 0 {
+            return None;
+        }
+
+        self.get_piece_at_pos(x, y - 1)
+    }
+
+    fn get_left_neighbour(&self, x: usize, y: usize) -> Option<&ImagePiece> {
+        if x == 0 {
+            return None;
+        }
+
+        self.get_piece_at_pos(x - 1, y)
+    }
+
+    fn get_bottom_neighbour(&self, x: usize, y: usize) -> Option<&ImagePiece> {
+        if y == self.width - 1 {
+            return None;
+        }
+
+        self.get_piece_at_pos(x, y + 1)
+    }
+
+    fn get_right_neighbour(&self, x: usize, y: usize) -> Option<&ImagePiece> {
+        if x == self.width - 1 {
+            return None;
+        }
+
+        self.get_piece_at_pos(x + 1, y)
+    }
+}
+
+pub fn next_pos(px: usize, py: usize, width: usize) -> Option<(usize, usize)> {
+    let i = py * width + px;
+    let ni = i + 1;
+
+    if ni >= width * width {
+        return None;
+    }
+
+    Some((ni % width, ni / width))
+}
+
+fn pieces_matching_borders(borders: &[Border], hm: &HashMap<Border, Vec<u32>>) -> HashSet<u32> {
+    let mut hs: HashSet<u32> = HashSet::new();
+
+    for border in borders {
+        let ids = &hm[border];
+        for id in ids {
+            hs.insert(*id);
+        }
+    }
+
+    hs
+}
+
+pub fn solveit(
+    image: &Image,
+    all_pieces: &[ImagePiece],
+    datas: &[ImageData],
+    hm: &HashMap<Border, Vec<u32>>,
+    used_images: &[u32],
+    px: usize,
+    py: usize,
+    order: usize,
+) -> Option<Image> {
+    let neighbour_borders = image.get_neiguhouring_borders(px, py, datas);
+    let mut pieces_to_find: Vec<u32> = pieces_matching_borders(&neighbour_borders, hm)
+        .into_iter()
+        .collect();
+
+    if px == 0 && py == 0 {
+        pieces_to_find = find_edges(all_pieces, datas, true)
+            .iter()
+            .map(|p| p.id)
+            .collect();
+    }
+
+    for id in pieces_to_find {
+        let piece = all_pieces.iter().find(|p| p.id == id).unwrap();
+        if used_images.contains(&piece.id) {
+            continue;
+        }
+
+        for (flip_v, flip_h) in &[(false, false), (true, false), (false, true)] {
+            for rot in &[
+                Rotation::Deg0,
+                Rotation::Deg90,
+                Rotation::Deg180,
+                Rotation::Deg270,
+            ] {
+                let mut pc = (*piece).clone();
+
+                pc.rotation = rot.clone();
+                pc.flip_h = *flip_h;
+                pc.flip_v = *flip_v;
+                if image.can_add_piece_at(px, py, &pc, datas) {
+                    let mut ic = image.clone();
+                    ic.set_piece_at_pos(px, py, pc);
+                    let mut new_used: Vec<u32> = used_images.to_vec();
+                    new_used.push(piece.id);
+                    match next_pos(px, py, image.width) {
+                        Some((nx, ny)) => {
+                            match solveit(&ic, all_pieces, datas, hm, &new_used, nx, ny, order + 1)
+                            {
+                                Some(im) => return Some(im),
+                                None => continue,
+                            }
+                        }
+                        None => {
+                            return Some(ic);
+                        }
+                    };
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn has_monster_at(image: &[Vec<bool>], px: usize, py: usize, rot: &Rotation, flip_v: bool, flip_h: bool) -> bool {
+    [
+        (0, 1),
+        (1, 2),
+        (4, 2),
+        (5, 1),
+        (6, 1),
+        (7, 2),
+        (10, 2),
+        (11, 1),
+        (12, 1),
+        (13, 2),
+        (16, 2),
+        (17, 1),
+        (18, 1),
+        (18, 0),
+        (19, 1),
+    ]
+    .iter()
+    .all(|(x, y)| {
+        let (fx,fy) = flip_horizontal(image.len(),flip_vertical(image.len(),rotate_coords(image.len(),(px + *x,py + *y),rot), flip_v), flip_h);
+
+        return match image.get(fy).and_then(|i| i.get(fx)) {
+            Some(b) => *b,
+            None => false,
+        };
+    })
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, datas) = parse_input(input).unwrap();
+
+    let pieces = datas
+        .iter()
+        .map(|d| ImagePiece {
+            id: d.id,
+            rotation: Rotation::Deg0,
+            flip_h: false,
+            flip_v: false,
+        })
+        .collect::<Vec<_>>();
+
+    // We asume the image is square
+    let image_width: usize = f64::sqrt(pieces.len() as f64) as usize;
+
+    let mut hm: HashMap<[bool; IMAGE_PIECE_SIZE], Vec<u32>> = HashMap::new();
+
+    for piece in &pieces {
+        let borders: [[bool; IMAGE_PIECE_SIZE]; 4] =
+            extract_borders(&piece, datas.iter().find(|d| d.id == piece.id).unwrap());
+
+        for border in borders.iter() {
+            match hm.clone().get(border) {
+                Some(v) => hm.insert(*border, merge_vecs(v.clone(), vec![piece.id])),
+                None => hm.insert(*border, vec![piece.id]),
+            };
+
+            match hm.clone().get(&flip_image_size_array(border)) {
+                Some(v) => hm.insert(
+                    flip_image_size_array(border),
+                    merge_vecs(v.clone(), vec![piece.id]),
+                ),
+                None => hm.insert(flip_image_size_array(border), vec![piece.id]),
+            };
+        }
+    }
+    let solution = solveit(&Image::new(image_width), &pieces, &datas, &hm, &[], 0, 0, 0).unwrap();
+
+    let image = solution.form_complete_image(&datas);
+
+    let mut n_monsters = 0;
+
+    'outer: for (flip_v, flip_h) in &[(false, false), (true, false), (false, true)] {
+        for rot in &[
+            Rotation::Deg0,
+            Rotation::Deg90,
+            Rotation::Deg180,
+            Rotation::Deg270,
+        ] {
+            n_monsters = 0;
+
+            for x in 0..image.len() {
+                for y in 0..image[x].len() {
+                    if has_monster_at(&image, x, y, rot, *flip_v, *flip_h) {
+                        n_monsters += 1;
+                    }
+                }
+            }
+
+            if n_monsters != 0 {
+                break 'outer;
+            }
+        }
+    }
+
+
+    let n_rough = image.into_iter().flatten().filter(|b| *b).count() - 15 * n_monsters;
+
+    n_rough.to_string()
 }
