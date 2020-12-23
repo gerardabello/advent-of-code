a637d80bc3c9430308430c4ac58cb96837f424d8
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-23 13:55:59 +0100

    add day 23 solution

--- src/d23p1.rs
+++ src/d23p1.rs
@@ -1,3 +1,101 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use std::collections::LinkedList;
+
+use nom::{
+    bytes::complete::take,
+    combinator::map_res,
+    multi::many1,
+    IResult,
+};
+
+// Implemented as a struct to avoid Copy, that way we can assure that we don't copy cups by
+// mistake.
+#[derive(Debug, Clone)]
+pub struct Cup {
+    pub number: u8,
+}
+
+pub fn parse_input(input: &str) -> IResult<&str, Vec<Cup>> {
+    many1(map_res(take(1 as usize), |s: &str| {
+        s.parse::<u8>().map(|number| Cup { number })
+    }))(input)
+}
+
+struct Game {
+    // The current cup is always the first one
+    cups: LinkedList<Cup>,
+}
+
+impl Game {
+    fn find_destination_index(&self, current_cup: &Cup) -> usize {
+        let mut possible = current_cup.number - 1;
+        loop {
+            if let Some(i) = self.cups.iter().position(|c| c.number == possible) {
+                return i;
+            }
+
+            if possible == 0 {
+                possible = 9;
+            }else {
+                possible -= 1;
+            }
+        }
+    }
+
+    fn cups_ordered_from_one(&self) -> LinkedList<Cup> {
+        let mut cups = self.cups.clone();
+
+        let mut from_one = cups.split_off(
+            cups.iter().position(|c| c.number == 1).unwrap()
+            );
+
+        from_one.append(&mut cups);
+
+        from_one
+    }
+
+    fn play_turn(&mut self) {
+        let current_cup = self
+            .cups
+            .pop_front()
+            .expect("There should be more than one cup");
+
+        let mut picked: LinkedList<Cup> = {
+            let mut tmp = LinkedList::new();
+
+            for _ in 0..3 {
+                tmp.push_back(
+                    self.cups
+                        .pop_front()
+                        .expect("There should be 3 cups to pick"),
+                );
+            }
+
+            tmp
+        };
+
+        let destination_index = self.find_destination_index(&current_cup);
+
+        let mut following = self.cups.split_off(destination_index + 1);
+
+        self.cups.append(&mut picked);
+        self.cups.append(&mut following);
+
+        // put current cup as last, so following cup is now current
+        self.cups.push_back(current_cup);
+
+    }
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, cups) = parse_input(input).unwrap();
+
+    let mut game = Game {
+        cups: cups.into_iter().collect(),
+    };
+
+    for _ in 0..100 {
+        game.play_turn();
+    }
+
+    game.cups_ordered_from_one().into_iter().skip(1).map(|c| c.number.to_string()).collect::<Vec<String>>().join("")
 }
--- src/d23p2.rs
+++ src/d23p2.rs
@@ -1,3 +1,97 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use nom::{bytes::complete::take, combinator::map_res, multi::many1, IResult};
+
+const MAX_NUM: usize = 1_000_000;
+
+type Cup = usize;
+
+pub fn parse_input(input: &str) -> IResult<&str, Vec<Cup>> {
+    many1(map_res(take(1 as usize), |s: &str| s.parse::<usize>()))(input)
+}
+
+struct CupRing(Vec<(usize, usize)>);
+
+impl CupRing {
+    fn new(cups: &[usize]) -> Self {
+        // As there is no "0" cup, we need to add a 0 element that will be never used
+        let mut ret = vec![(0, 0); cups.len() + 1];
+
+        for w in cups.windows(3) {
+            ret[w[1]] = (w[0], w[2]);
+        }
+
+        ret[cups[0]] = (cups[cups.len() - 1], cups[1]);
+        ret[cups[cups.len() - 1]] = (cups[cups.len() - 2], cups[0]);
+
+        Self(ret)
+    }
+
+    fn get_next(&self, cup: Cup) -> Cup {
+        self.0[cup].1
+    }
+
+    fn set_next(&mut self, one: Cup, next: Cup) {
+        self.0[one].1 = next;
+        self.0[next].0 = one;
+    }
+}
+
+struct Game {
+    ring: CupRing,
+    current: Cup,
+}
+
+impl Game {
+    fn play_turn(&mut self) {
+        let mut picked = Vec::new();
+        picked.push(self.ring.0[self.current].1);
+        picked.push(self.ring.0[picked[0]].1);
+        picked.push(self.ring.0[picked[1]].1);
+
+        let mut possible = self.current - 1;
+
+        loop {
+            if possible == 0 {
+                possible = MAX_NUM;
+            }
+
+            if picked.contains(&possible) {
+                possible -= 1;
+            } else {
+                break;
+            }
+        }
+
+        let destination = possible;
+
+        let destination_next = self.ring.get_next(destination);
+        let picked_next = self.ring.get_next(picked[2]);
+
+        self.ring.set_next(self.current, picked_next);
+
+        self.ring.set_next(destination, picked[0]);
+        self.ring.set_next(picked[2], destination_next);
+
+        self.current = self.ring.get_next(self.current);
+    }
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, mut cups) = parse_input(input).unwrap();
+
+    for i in cups.len()..MAX_NUM as usize {
+        cups.push(i + 1)
+    }
+
+    assert!(cups.len() == MAX_NUM as usize);
+
+    let mut game = Game {
+        ring: CupRing::new(&cups),
+        current: cups[0],
+    };
+
+    for _ in 1..10_000_000 {
+        game.play_turn();
+    }
+
+    (game.ring.get_next(1) * game.ring.get_next(game.ring.get_next(1))).to_string()
 }
