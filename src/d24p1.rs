b0fe38cdf27c28be41cb645d97313e37e2a829db
Gerard Abelló Serras <gerardabello@gmail.com>
2020-12-24 09:14:00 +0100

    add day 24 solution

--- src/d24p1.rs
+++ src/d24p1.rs
@@ -1,3 +1,96 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    multi::{many1, separated_list1},
+    IResult,
+};
+use std::collections::HashMap;
+
+pub enum Direction {
+    East,
+    SouthEast,
+    SouthWest,
+    West,
+    NorthWest,
+    NorthEast,
+}
+
+fn parse_direction(input: &str) -> IResult<&str, Direction> {
+    let (input, s) = alt((
+        tag("e"),
+        tag("w"),
+        tag("sw"),
+        tag("nw"),
+        tag("se"),
+        tag("ne"),
+    ))(input)?;
+
+    let dir = match s {
+        "e" => Direction::East,
+        "w" => Direction::West,
+        "se" => Direction::SouthEast,
+        "ne" => Direction::NorthEast,
+        "sw" => Direction::SouthWest,
+        "nw" => Direction::NorthWest,
+        _ => unreachable!(),
+    };
+
+    Ok((input, dir))
+}
+
+pub fn parse_input(input: &str) -> IResult<&str, Vec<Vec<Direction>>> {
+    separated_list1(tag("\n"), many1(parse_direction))(input)
+}
+
+pub fn dir_to_hex_coords(dir: &Direction) -> (isize, isize) {
+    match dir {
+        Direction::East => (1, 0),
+        Direction::West => (-1, 0),
+
+        Direction::NorthEast => (0, 1),
+        Direction::NorthWest => (-1, 1),
+        Direction::SouthWest => (0, -1),
+        Direction::SouthEast => (1, -1),
+    }
+}
+
+fn add_dir(dir: &Direction, coords: (isize, isize)) -> (isize, isize) {
+    let relative_coords = dir_to_hex_coords(dir);
+
+    (coords.0 + relative_coords.0, coords.1 + relative_coords.1)
+}
+
+pub fn dirs_to_hex_coords(dirs: &[Direction]) -> (isize, isize) {
+    dirs.iter().fold((0, 0), |acc, dir| add_dir(dir, acc))
+}
+
+fn count_slice_by_key<T: Clone + std::hash::Hash + std::cmp::Eq>(
+    slice: &[T],
+) -> HashMap<T, usize> {
+    let mut hm: HashMap<T, usize> = HashMap::new();
+    for v in slice {
+        if hm.contains_key(v) {
+            hm.insert(v.clone(), hm.get(v).unwrap() + 1);
+        } else {
+            hm.insert(v.clone(), 0);
+        }
+    }
+
+    hm
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, directions) = parse_input(input).unwrap();
+    let coords: Vec<(isize, isize)> = directions
+        .into_iter()
+        .map(|dirs| dirs_to_hex_coords(&dirs))
+        .collect();
+
+    let counts = count_slice_by_key(&coords);
+
+    counts
+        .iter()
+        .filter(|(_, v)| *v % 2 == 0)
+        .count()
+        .to_string()
 }
--- src/d24p2.rs
+++ src/d24p2.rs
@@ -1,3 +1,104 @@
-pub fn solve(_input: &str) -> String {
-    panic!("Not implemented");
+use crate::d24p1::{dir_to_hex_coords, dirs_to_hex_coords, parse_input, Direction};
+
+#[derive(Clone)]
+struct Floor {
+    tiles: Vec<Vec<bool>>,
+    radius: usize,
+}
+
+impl Floor {
+    fn new(radius: usize) -> Self {
+        let vec_size = radius * 2 + 1;
+        Self {
+            radius,
+            tiles: vec![vec![false; vec_size]; vec_size],
+        }
+    }
+
+    fn tile_coord(&self, coord: (isize, isize)) -> (usize, usize) {
+        if coord.0 < -(self.radius as isize) || coord.0 > self.radius as isize {
+            panic!("invalid coordinates {:?}", coord);
+        }
+        if coord.1 < -(self.radius as isize) || coord.1 > self.radius as isize {
+            panic!("invalid coordinates {:?}", coord);
+        }
+
+        (
+            (coord.0 + self.radius as isize) as usize,
+            (coord.1 + self.radius as isize) as usize,
+        )
+    }
+
+    fn get_tile(&self, coord: (isize, isize)) -> bool {
+        let tc = self.tile_coord(coord);
+
+        self.tiles[tc.1][tc.0]
+    }
+
+    fn flip_tile(&mut self, coord: (isize, isize)) {
+        let tc = self.tile_coord(coord);
+
+        self.tiles[tc.1][tc.0] = !self.tiles[tc.1][tc.0]
+    }
+
+    fn n_black_tiles(&self) -> usize {
+        self.tiles.iter().flatten().filter(|t| **t).count()
+    }
+
+    fn n_neighbour_black_tiles(&self, coord: (isize, isize)) -> usize {
+        [
+            Direction::East,
+            Direction::West,
+            Direction::SouthEast,
+            Direction::NorthEast,
+            Direction::SouthWest,
+            Direction::NorthWest,
+        ]
+        .iter()
+        .map(dir_to_hex_coords)
+        .map(|(dx, dy)| self.get_tile((coord.0 + dx, coord.1 + dy)))
+        .filter(|v| *v)
+        .count()
+    }
+
+    fn do_flipping(&mut self) {
+        let margin = 1;
+        let freezed_clone = self.clone();
+
+        for x in -(self.radius as isize) + margin..(self.radius as isize) + 1 - margin {
+            for y in -(self.radius as isize) + margin..(self.radius as isize) + 1 - margin {
+                let nb = freezed_clone.n_neighbour_black_tiles((x, y));
+                let t = freezed_clone.get_tile((x, y));
+
+                if t && (nb == 0 || nb > 2) {
+                    self.flip_tile((x, y));
+                }
+
+                if !t && (nb == 2) {
+                    self.flip_tile((x, y));
+                }
+            }
+        }
+    }
+}
+
+pub fn solve(input: &str) -> String {
+    let (_, directions) = parse_input(input).unwrap();
+    let coords: Vec<(isize, isize)> = directions
+        .into_iter()
+        .map(|dirs| dirs_to_hex_coords(&dirs))
+        .collect();
+
+    let mut floor = Floor::new(100);
+
+    for (x, y) in coords {
+        floor.flip_tile((x, y));
+    }
+
+    for day in 1..101 {
+        floor.do_flipping();
+        //println!("Day {}: {}", day, floor.n_black_tiles());
+    }
+
+    floor.n_black_tiles().to_string()
 }
