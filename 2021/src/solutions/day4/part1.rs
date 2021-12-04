49c2f6f6fb917800adb197f435a30b593572f3c1
Gerard Abelló Serras <gerardabello@gmail.com>
2021-12-04 08:43:51 +0100

    day4

--- 2021/src/solutions/day4/part1.rs
+++ 2021/src/solutions/day4/part1.rs
@@ -1,13 +1,75 @@
-use nom::IResult;
+use nom::{
+    bytes::complete::tag,
+    multi::{many0, many1, separated_list1},
+    sequence::{preceded, tuple},
+    IResult,
+};
 
+use crate::matrix::transpose;
 use crate::parsers::{full, lines, unsigned_int};
 
-pub fn parse_line(input: &str) -> IResult<&str, usize> {
-    unsigned_int::<usize>(input)
+pub type Board = Vec<Vec<usize>>;
+pub type BoardSlice<'a> = &'a[Vec<usize>];
+
+fn parse_calls(input: &str) -> IResult<&str, Vec<usize>> {
+    separated_list1(tag(","), unsigned_int::<usize>)(input)
+}
+
+fn parse_board(input: &str) -> IResult<&str, Board> {
+    lines(preceded(
+        many0(tag(" ")),
+        separated_list1(many1(tag(" ")), unsigned_int::<usize>),
+    ))(input)
+}
+
+fn parse_boards(input: &str) -> IResult<&str, Vec<Board>> {
+    separated_list1(tag("\n\n"), parse_board)(input)
+}
+
+pub fn parse_bingo(input: &str) -> IResult<&str, (Vec<usize>, Vec<Board>)> {
+    let (input, (calls, _, boards)) = tuple((parse_calls, tag("\n\n"), parse_boards))(input)?;
+    Ok((input, (calls, boards)))
+}
+
+pub fn board_score(calls: &[usize], board: BoardSlice) -> usize {
+    let mut sum_of_unmarked: usize = 0;
+    for row in board {
+        sum_of_unmarked += row.iter().filter(|v| !calls.contains(v)).sum::<usize>();
+    }
+
+    let last_call = calls[calls.len() - 1];
+
+
+    sum_of_unmarked * last_call
+}
+
+pub fn check_board(calls: &[usize], board: BoardSlice) -> bool {
+    for row in board {
+        if row.iter().all(|v| calls.contains(v)) {
+            return true;
+        }
+    }
+
+    for row in transpose(board.to_owned()) {
+        if row.iter().all(|v| calls.contains(v)) {
+            return true;
+        }
+    }
+
+    false
 }
 
 pub fn solve(input: &str) -> usize {
-    let (_, _) = full(lines(parse_line))(input).unwrap();
+    let (_, (calls, boards)) = full(parse_bingo)(input).unwrap();
 
-    panic!("Not implemented");
+    for call_idx in 1..calls.len() {
+        let tmp_calls = &calls[0..call_idx];
+        for board in &boards {
+            if check_board(tmp_calls, board) {
+                return board_score(tmp_calls, board);
+            }
+        }
+    }
+
+    panic!("Not winner found");
 }
--- 2021/src/solutions/day4/part2.rs
+++ 2021/src/solutions/day4/part2.rs
@@ -1,13 +1,27 @@
-use nom::IResult;
-
-use crate::parsers::{full, lines, unsigned_int};
-
-pub fn parse_line(input: &str) -> IResult<&str, usize> {
-    unsigned_int::<usize>(input)
-}
+use crate::parsers::full;
+use crate::solutions::day4::part1::{board_score, check_board, parse_bingo, Board};
 
 pub fn solve(input: &str) -> usize {
-    let (_, _) = full(lines(parse_line))(input).unwrap();
+    let (_, (calls, boards)) = full(parse_bingo)(input).unwrap();
 
-    panic!("Not implemented");
+    let mut losing_boards = boards;
+
+    for call_idx in 1..calls.len() {
+        let tmp_calls = &calls[0..call_idx];
+        losing_boards = losing_boards
+            .into_iter()
+            .filter(|board| !check_board(tmp_calls, board))
+            .collect::<Vec<Board>>();
+        if losing_boards.len() == 1 {
+            let last_board = &losing_boards[0];
+            for last_call_idx in call_idx..calls.len() {
+                let last_tmp_calls = &calls[0..last_call_idx];
+                if check_board(last_tmp_calls, last_board) {
+                    return board_score(last_tmp_calls, last_board);
+                }
+            }
+        }
+    }
+
+    panic!("There is no ONE losing board");
 }
