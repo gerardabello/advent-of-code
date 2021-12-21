eacc799ee43ca374997616855bcf6bcd6f89c3f8
Gerard Abelló Serras <gerardabello@gmail.com>
2021-12-21 08:35:31 +0100

    day21

--- 2021/src/solutions/day21/part1.rs
+++ 2021/src/solutions/day21/part1.rs
@@ -1,11 +1,64 @@
-use crate::parsers::{full, lines, unsigned_int};
+use crate::parsers::{full, unsigned_int};
+use nom::{bytes::complete::tag, IResult};
 
-pub fn parse_input(input: &str) -> Vec<usize> {
-    full(lines(unsigned_int::<usize>))(input).unwrap().1
+
+pub struct Dice {
+    pub times: usize
+}
+
+impl Dice {
+    pub fn throw(&mut self) -> usize {
+        self.times += 1;
+        self.times
+    }
+}
+
+pub fn parse_input_res(input: &str) -> IResult<&str, [usize; 2]> {
+    let (input, _) = tag("Player 1 starting position: ")(input)?;
+    let (input, p1) = unsigned_int::<usize>(input)?;
+    let (input, _) = tag("\n")(input)?;
+    let (input, _) = tag("Player 2 starting position: ")(input)?;
+    let (input, p2) = unsigned_int::<usize>(input)?;
+    Ok((input, [p1, p2]))
+}
+
+pub fn parse_input(input: &str) -> [usize; 2] {
+    full(parse_input_res)(input).unwrap().1
+}
+
+pub fn step(position: &mut usize, score: &mut usize,dice: &mut Dice)
+{
+    let result = dice.throw() + dice.throw() + dice.throw();
+    *position = ((*position + result - 1) % 10) + 1;
+    *score += *position
 }
 
 pub fn solve(input: &str) -> usize {
-    let _ = parse_input(input);
+    let starting_positions = parse_input(input);
 
-    panic!("Not implemented");
+    let mut p1_pos = starting_positions[0];
+    let mut p2_pos = starting_positions[1];
+
+    let mut p1_score = 0;
+    let mut p2_score = 0;
+
+    let mut p1_turn = true;
+
+    let mut dice = Dice{times: 0};
+
+    loop {
+        if p1_turn {
+            step(&mut p1_pos, &mut p1_score, &mut dice);
+            if p1_score >= 1000 {
+                return p2_score * dice.times;
+            }
+        } else {
+            step(&mut p2_pos, &mut p2_score, &mut dice);
+            if p2_score >= 1000 {
+                return p1_score * dice.times;
+            }
+        }
+
+        p1_turn = !p1_turn;
+    }
 }
--- 2021/src/solutions/day21/part2.rs
+++ 2021/src/solutions/day21/part2.rs
@@ -1,7 +1,98 @@
 use super::part1::parse_input;
+use std::collections::HashMap;
 
-pub fn solve(input: &str) -> usize {
-    let _ = parse_input(input);
+type Score = usize;
+type Position = usize;
 
-    panic!("Not implemented");
+const THROWS: [usize; 27] = [
+    1 + 1 + 1,
+    1 + 1 + 2,
+    1 + 1 + 3,
+    1 + 2 + 1,
+    1 + 2 + 2,
+    1 + 2 + 3,
+    1 + 3 + 1,
+    1 + 3 + 2,
+    1 + 3 + 3,
+    2 + 1 + 1,
+    2 + 1 + 2,
+    2 + 1 + 3,
+    2 + 2 + 1,
+    2 + 2 + 2,
+    2 + 2 + 3,
+    2 + 3 + 1,
+    2 + 3 + 2,
+    2 + 3 + 3,
+    3 + 1 + 1,
+    3 + 1 + 2,
+    3 + 1 + 3,
+    3 + 2 + 1,
+    3 + 2 + 2,
+    3 + 2 + 3,
+    3 + 3 + 1,
+    3 + 3 + 2,
+    3 + 3 + 3,
+];
+
+fn new_position(position: Position, dice_result: usize) -> Position {
+    ((position + dice_result - 1) % 10) + 1
+}
+fn new_score(score: Score, new_position: Position) -> Score {
+    score + new_position
+}
+
+fn how_many_times_each_wins(
+    cache: &mut HashMap<([Position; 2], [Score; 2], bool), [u128; 2]>,
+    positions: &[Position; 2],
+    scores: &[Score; 2],
+    p1_turn: bool,
+) -> [u128; 2] {
+    match cache.get(&(*positions, *scores, p1_turn)) {
+        Some(winners) => *winners,
+        None => {
+            let mut winners: [u128; 2] = [0, 0];
+            let player_index = if p1_turn { 0 } else { 1 };
+            for throw in THROWS {
+                let new_pos = new_position(positions[player_index], throw);
+                let new_scr = new_score(scores[player_index], new_pos);
+
+                if new_scr >= 21 {
+                    winners[player_index] += 1;
+                } else {
+                    let universe_winners = if p1_turn {
+                        how_many_times_each_wins(
+                            cache,
+                            &[new_pos, positions[1]],
+                            &[new_scr, scores[1]],
+                            false,
+                        )
+                    } else {
+                        how_many_times_each_wins(
+                            cache,
+                            &[positions[0], new_pos],
+                            &[scores[0], new_scr],
+                            true,
+                        )
+                    };
+
+                    winners[0] += universe_winners[0];
+                    winners[1] += universe_winners[1];
+                }
+            }
+
+            cache.insert((*positions, *scores, p1_turn), winners);
+            winners
+        }
+    }
+}
+
+pub fn solve(input: &str) -> u128 {
+    let starting_positions = parse_input(input);
+
+    let mut cache = HashMap::new();
+
+    how_many_times_each_wins(&mut cache, &starting_positions, &[0, 0], true)
+        .into_iter()
+        .max()
+        .unwrap()
 }
