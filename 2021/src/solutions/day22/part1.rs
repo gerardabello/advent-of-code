50c96c78d321a1fb2e398613b0508026fc575c1a
Gerard Abelló Serras <gerardabello@gmail.com>
2021-12-22 10:48:25 +0100

    day22

--- 2021/src/solutions/day22/part1.rs
+++ 2021/src/solutions/day22/part1.rs
@@ -1,11 +1,92 @@
-use crate::parsers::{full, lines, unsigned_int};
+use std::collections::HashSet;
+use std::ops::Range;
 
-pub fn parse_input(input: &str) -> Vec<usize> {
-    full(lines(unsigned_int::<usize>))(input).unwrap().1
+use nom::{branch::alt, bytes::complete::tag, combinator::map, IResult};
+
+use crate::parsers::{full, lines, signed_int};
+
+pub type Cuboid = [Range<isize>; 3];
+
+#[derive(Clone, Debug)]
+pub enum Step {
+    On(Cuboid),
+    Off(Cuboid),
+}
+
+fn parse_range(input: &str) -> IResult<&str, Range<isize>> {
+    let (input, start) = signed_int::<isize>(input)?;
+    let (input, _) = tag("..")(input)?;
+    let (input, end) = signed_int::<isize>(input)?;
+
+    Ok((input, start..end + 1))
+}
+
+fn parse_line(input: &str) -> IResult<&str, Step> {
+    let (input, on) = alt((map(tag("on"), |_| true), map(tag("off"), |_| false)))(input)?;
+    let (input, _) = tag(" ")(input)?;
+
+    let (input, _) = tag("x=")(input)?;
+    let (input, range_x) = parse_range(input)?;
+    let (input, _) = tag(",")(input)?;
+    let (input, _) = tag("y=")(input)?;
+    let (input, range_y) = parse_range(input)?;
+    let (input, _) = tag(",")(input)?;
+    let (input, _) = tag("z=")(input)?;
+    let (input, range_z) = parse_range(input)?;
+
+    let cuboid = [range_x, range_y, range_z];
+    Ok((
+        input,
+        match on {
+            true => Step::On(cuboid),
+            false => Step::Off(cuboid),
+        },
+    ))
+}
+
+pub fn parse_input(input: &str) -> Vec<Step> {
+    full(lines(parse_line))(input).unwrap().1
+}
+
+fn check_cuboid(cuboid: &Cuboid) -> bool {
+    cuboid[0].start >= -50
+        && cuboid[0].end <= 51
+        && cuboid[1].start >= -50
+        && cuboid[1].end <= 51
+        && cuboid[2].start >= -50
+        && cuboid[2].end <= 51
 }
 
 pub fn solve(input: &str) -> usize {
-    let _ = parse_input(input);
+    let steps = parse_input(input);
 
-    panic!("Not implemented");
+    let mut hs = HashSet::new();
+
+    for step in steps.iter().filter(|step| match step {
+        Step::On(cuboid) => check_cuboid(cuboid),
+        Step::Off(cuboid) => check_cuboid(cuboid),
+    }) {
+        match step {
+            Step::On(cuboid) => {
+                for x in cuboid[0].clone() {
+                    for y in cuboid[1].clone() {
+                        for z in cuboid[2].clone() {
+                            hs.insert([x, y, z]);
+                        }
+                    }
+                }
+            }
+            Step::Off(cuboid) => {
+                for x in cuboid[0].clone() {
+                    for y in cuboid[1].clone() {
+                        for z in cuboid[2].clone() {
+                            hs.remove(&[x, y, z]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    hs.len()
 }
--- 2021/src/solutions/day22/part2.rs
+++ 2021/src/solutions/day22/part2.rs
@@ -1,7 +1,74 @@
-use super::part1::parse_input;
+use std::collections::HashMap;
+use std::ops::Range;
 
-pub fn solve(input: &str) -> usize {
-    let _ = parse_input(input);
+use super::part1::{parse_input, Cuboid, Step};
 
-    panic!("Not implemented");
+pub fn cuboid_area(cuboid: &Cuboid) -> u128 {
+    (cuboid[0].start - cuboid[0].end).abs() as u128
+        * (cuboid[1].start - cuboid[1].end).abs() as u128
+        * (cuboid[2].start - cuboid[2].end).abs() as u128
+}
+
+pub fn cuboid_interesection(c1: &Cuboid, c2: &Cuboid) -> Option<Cuboid> {
+    let range_x = Range {
+        start: isize::max(c1[0].start, c2[0].start),
+        end: isize::min(c1[0].end, c2[0].end),
+    };
+
+    if range_x.start >= range_x.end {
+        return None;
+    };
+
+    let range_y = Range {
+        start: isize::max(c1[1].start, c2[1].start),
+        end: isize::min(c1[1].end, c2[1].end),
+    };
+
+    if range_y.start >= range_y.end {
+        return None;
+    };
+
+    let range_z = Range {
+        start: isize::max(c1[2].start, c2[2].start),
+        end: isize::min(c1[2].end, c2[2].end),
+    };
+
+    if range_z.start >= range_z.end {
+        return None;
+    };
+
+    Some([range_x, range_y, range_z])
+}
+
+pub fn get_cuboid(step: &Step) -> Cuboid {
+    match step {
+        Step::On(cuboid) => cuboid.clone(),
+        Step::Off(cuboid) => cuboid.clone(),
+    }
+}
+
+pub fn solve(input: &str) -> u128 {
+    let steps = parse_input(input);
+
+    let mut cuboid_with_weights  : HashMap<Cuboid, i128> = HashMap::new();
+
+    for step in steps {
+        let mut new_cuboid_with_weights = cuboid_with_weights.clone();
+        let cuboid = get_cuboid(&step);
+        for (c2, weight) in cuboid_with_weights {
+            if let Some(intersection) = cuboid_interesection(&c2, &cuboid) {
+                *new_cuboid_with_weights.entry(intersection).or_insert(0) -= weight;
+            }
+        }
+
+        if matches!(step, Step::On(_)) {
+            *new_cuboid_with_weights.entry(cuboid).or_insert(0) += 1;
+        }
+        cuboid_with_weights = new_cuboid_with_weights;
+    }
+
+    cuboid_with_weights
+        .into_iter()
+        .map(|(cuboid, weight)| cuboid_area(&cuboid) as i128 * weight)
+        .sum::<i128>() as u128
 }
